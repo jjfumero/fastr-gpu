4344e11ab496659d88b8ddcd0709894a935a33c6
Juan Fumero <juan.fumero@ed.ac.uk>
2016-03-29 18:54:06 +0100

    benchmark updated

--- examples/xmap.R
+++ examples/xmap.R
@@ -27,4 +27,31 @@ end <- nanotime()
 print(end-start);
 
 
+sumArrays2 <- function(x) {
+	a <- 0;
+	for (i in seq(1,1000)) {
+		a <- x * 2.0;
+	}
+	return(a);
+}
+
+print("Sapply1")
+start <- nanotime()
+r <- sapply(a, sumArrays2);
+end <- nanotime()
+print(end-start);
+
+
+print("Sapply2")
+start <- nanotime()
+r <- sapply(a, sumArrays2);
+end <- nanotime()
+print(end-start);
+
+print("Sapply3")
+start <- nanotime()
+r <- sapply(a, sumArrays2);
+end <- nanotime()
+print(end-start);
+
 #print(result) 
