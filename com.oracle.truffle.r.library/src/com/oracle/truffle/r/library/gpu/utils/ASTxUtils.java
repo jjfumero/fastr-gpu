d67dd885ee93671d0ee4f9de2fc36f04d3956ef9
Juan Fumero <juan.fumero@ed.ac.uk>
2016-04-04 15:00:06 +0100

    Support to Tuple2<> to Tuple4<>

--- com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/utils/ASTxUtils.java
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/utils/ASTxUtils.java
@@ -3,7 +3,10 @@ package com.oracle.truffle.r.library.gpu.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import uk.ac.ed.datastructures.tuples.Tuple;
 import uk.ac.ed.datastructures.tuples.Tuple2;
+import uk.ac.ed.datastructures.tuples.Tuple3;
+import uk.ac.ed.datastructures.tuples.Tuple4;
 
 import com.oracle.truffle.api.source.SourceSection;
 import com.oracle.truffle.r.runtime.ArgumentsSignature;
@@ -89,19 +92,34 @@ public class ASTxUtils {
         return argsPackage;
     }
 
+    /**
+     * Prepare args for the function with varargs
+     *
+     * @param nArgs
+     * @param function
+     * @param input
+     * @param nameArgs
+     * @return Object[]
+     */
     @SuppressWarnings("rawtypes")
     public static Object[] getArgsPackage(int nArgs, RFunction function, Object input, String[] nameArgs) {
-        // prepare args for the function with varargs
-        Object[] argsRFunction = new Object[nArgs];
 
-        if (input instanceof Tuple2) {
+        Object[] argsRFunction = new Object[nArgs];
+        if (!(input instanceof Tuple)) {
+            argsRFunction[0] = input;
+        } else if (input instanceof Tuple2) {
             argsRFunction[0] = ((Tuple2) input)._1();
             argsRFunction[1] = ((Tuple2) input)._2();
-        } else {
-            argsRFunction[0] = input;
+        } else if (input instanceof Tuple3) {
+            argsRFunction[0] = ((Tuple3) input)._1();
+            argsRFunction[1] = ((Tuple3) input)._2();
+            argsRFunction[2] = ((Tuple3) input)._3();
+        } else if (input instanceof Tuple4) {
+            argsRFunction[0] = ((Tuple4) input)._1();
+            argsRFunction[1] = ((Tuple4) input)._2();
+            argsRFunction[2] = ((Tuple4) input)._3();
+            argsRFunction[3] = ((Tuple4) input)._4();
         }
-
-        // Create the package
         Object[] argsPackage = RArguments.create(function, null, null, 0, argsRFunction, ArgumentsSignature.get(nameArgs), null);
         return argsPackage;
     }
