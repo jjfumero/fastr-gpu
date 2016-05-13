1c41cf3f540e5ec08bb1a09cb6c639b8f5936cca
Juan Fumero <juan.fumero@ed.ac.uk>
2016-05-13 17:36:40 +0100

    Marked deleted disabled in GPU compilation phase

--- com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/phases/GPUCleanPhase.java
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/phases/GPUCleanPhase.java
@@ -1,10 +1,15 @@
 package com.oracle.truffle.r.library.gpu.phases;
 
 import com.oracle.graal.graph.Node;
+import com.oracle.graal.nodes.ConstantNode;
+import com.oracle.graal.nodes.ParameterNode;
 import com.oracle.graal.nodes.StructuredGraph;
+import com.oracle.graal.nodes.calc.IntegerEqualsNode;
 import com.oracle.graal.nodes.calc.ReinterpretNode;
 import com.oracle.graal.nodes.util.GraphUtil;
 import com.oracle.graal.phases.Phase;
+import com.oracle.graal.phases.common.CanonicalizerPhase;
+import com.oracle.graal.phases.common.DeadCodeEliminationPhase;
 
 public class GPUCleanPhase extends Phase {
 
@@ -13,20 +18,25 @@ public class GPUCleanPhase extends Phase {
 
         System.out.println("Compilation GPU PHASE");
 
+        ParameterNode parameterNode = graph.getParameter(1);
+
+        System.out.println(GraphUtil.unproxify((parameterNode.asNode())));
+
         for (Node node : graph.getNodes()) {
-            System.out.println(">> Node: " + node);
+            System.out.println(">> Node: " + node.getClass());
 
             if (node instanceof ReinterpretNode) {
                 System.out.println("ReinterpretNode NODE !!!!!!!!!!!!");
-
-                ReinterpretNode n = (ReinterpretNode) node;
-                System.out.println(n.getValue());
-                GraphUtil.propagateKill(n);
+                // node.markDeleted();
+            } else if (node instanceof ConstantNode) {
+                // node.markDeleted();
             }
 
             for (Node n : node.cfgSuccessors()) {
                 System.out.println("\t " + n);
             }
         }
+
+        new DeadCodeEliminationPhase().apply(graph);
     }
 }
