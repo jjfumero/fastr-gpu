8f677009f4f840073a269e17a047dde3f834be4c
Juan Fumero <juan.fumero@ed.ac.uk>
2016-05-17 16:51:25 +0100

    GPUFrameStateEliminationPhase added

---
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/phases/GPUFrameStateElimination.java
@@ -0,0 +1,30 @@
+package com.oracle.truffle.r.library.gpu.phases;
+
+import uk.ac.ed.marawacc.graal.GraalOCLBackendConnector;
+
+import com.oracle.graal.nodes.FrameState;
+import com.oracle.graal.nodes.StructuredGraph;
+import com.oracle.graal.phases.Phase;
+import com.oracle.graal.phases.common.CanonicalizerPhase;
+import com.oracle.graal.phases.common.DeadCodeEliminationPhase;
+import com.oracle.graal.phases.tiers.PhaseContext;
+import com.oracle.graal.phases.util.Providers;
+
+public class GPUFrameStateElimination extends Phase {
+
+    @Override
+    protected void run(StructuredGraph graph) {
+
+        System.out.println("GPUFrameStateElimination");
+
+        for (FrameState frameStateNode : graph.getNodes(FrameState.TYPE)) {
+            frameStateNode.replaceAtUsages(null);
+            frameStateNode.safeDelete();
+        }
+
+        Providers providers = GraalOCLBackendConnector.getProviders();
+
+        new CanonicalizerPhase().apply(graph, new PhaseContext(providers));
+        new DeadCodeEliminationPhase().apply(graph);
+    }
+}
