2421ac31983221b3de53c91ae5cfc5b512e6a1a0
Juan Fumero <juan.fumero@ed.ac.uk>
2016-05-19 10:16:18 +0100

    Boxing elimination phase provided for the GPU

--- com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/phases/GPUBoxingEliminationPhase.java
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/phases/GPUBoxingEliminationPhase.java
@@ -14,30 +14,38 @@ import com.oracle.graal.phases.common.ConditionalEliminationPhase;
 import com.oracle.graal.phases.common.DeadCodeEliminationPhase;
 import com.oracle.graal.phases.common.inlining.InliningPhase;
 import com.oracle.graal.phases.tiers.HighTierContext;
+import com.oracle.graal.phases.tiers.PhaseContext;
 import com.oracle.graal.phases.util.Providers;
 import com.oracle.graal.virtual.phases.ea.PartialEscapePhase;
 
 public class GPUBoxingEliminationPhase extends Phase {
 
-    protected static PhaseSuite<HighTierContext> getDefaultGraphBuilderSuite(Backend backend) {
+    private static PhaseSuite<HighTierContext> getDefaultGraphBuilderSuite(Backend backend) {
         return backend.getSuites().getDefaultGraphBuilderSuite().copy();
     }
 
-    @Override
-    protected void run(StructuredGraph graph) {
+    /**
+     * Boxing elimination phase.
+     *
+     * @param graph
+     * @param context
+     * @param providers
+     */
+    @SuppressWarnings("unused")
+    private static void boxingRemoval(StructuredGraph graph, HighTierContext context, Providers providers) {
+        new InliningPhase(new CanonicalizerPhase()).apply(graph, context);
+        new PartialEscapePhase(false, new CanonicalizerPhase()).apply(graph, context);
+        new CanonicalizerPhase().apply(graph, new PhaseContext(providers));
+        new DeadCodeEliminationPhase().apply(graph);
+    }
 
-        System.out.println("GPUBoxingElimination phase");
-
-        Backend backend = GraalOCLBackendConnector.getHostBackend();
-        Providers providers = backend.getProviders();
-        PhaseSuite<HighTierContext> graphBuilderSuite = getDefaultGraphBuilderSuite(backend);
-
-        HighTierContext context = new HighTierContext(providers, graphBuilderSuite, OptimisticOptimizations.ALL);
-// new InliningPhase(new CanonicalizerPhase()).apply(graph, context);
-// new PartialEscapePhase(false, new CanonicalizerPhase()).apply(graph, context);
-// new CanonicalizerPhase().apply(graph, new PhaseContext(providers));
-// new DeadCodeEliminationPhase().apply(graph);
-
+    /**
+     * Another way of boxing elimination.
+     *
+     * @param graph
+     * @param context
+     */
+    private static void boxingRemoval2(StructuredGraph graph, HighTierContext context) {
         CanonicalizerPhase canonicalizer = new CanonicalizerPhase();
         canonicalizer.apply(graph, context);
         new InliningPhase(new CanonicalizerPhase()).apply(graph, context);
@@ -51,5 +59,19 @@ public class GPUBoxingEliminationPhase extends Phase {
         new ConditionalEliminationPhase().apply(graph);
         new DeadCodeEliminationPhase().apply(graph);
         canonicalizer.apply(graph, context);
+    }
+
+    @Override
+    protected void run(StructuredGraph graph) {
+
+        System.out.println("GPUBoxingElimination phase");
+
+        Backend backend = GraalOCLBackendConnector.getHostBackend();
+        Providers providers = backend.getProviders();
+        PhaseSuite<HighTierContext> graphBuilderSuite = getDefaultGraphBuilderSuite(backend);
+
+        HighTierContext context = new HighTierContext(providers, graphBuilderSuite, OptimisticOptimizations.ALL);
+
+        boxingRemoval2(graph, context);
     }
 }
