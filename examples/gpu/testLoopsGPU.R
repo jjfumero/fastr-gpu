74f791fb48dc55972893867214ab192abda4bdda
Juan Fumero <juan.fumero@ed.ac.uk>
2016-06-10 17:35:20 +0100

    Loops 1 - resolved because of constant propagation

---
+++ examples/gpu/testLoopsGPU.R
@@ -0,0 +1,51 @@
+a <- 1:100000
+
+f <- function(x) {
+	a <- 1
+	for (i in seq(1:10)) {
+		b <- a * i
+		a <- x + b
+	}
+	return (a)
+}
+
+g <- function(x) {
+	a <- 1
+	for (i in seq(1:10)) {
+		b <- a * i
+		a <- x + b
+	}
+	return (a)
+}
+
+
+seqResult <- sapply(a, f)
+
+for (i in seq(1,20)) {
+
+    start <- nanotime()
+    result <- marawacc.testGPU(a, f)
+    end <- nanotime()
+    print(end-start);
+	
+	nonError <- identical(seqResult, result)
+	if (!nonError) {
+		for (i in seq(result)) {
+			if (abs(seqResult[i] - result[i]) > 0.1) {
+				print(nonError)
+				break;
+			}
+		}	
+	} else {
+		print(nonError) 
+	}
+
+}
+
+for (i in seq(1,20)) {
+    start <- nanotime()
+    result <- sapply(a, g)
+    end <- nanotime()
+    print(end-start);
+}
+
