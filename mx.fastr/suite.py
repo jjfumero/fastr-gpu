f4e17b6e0b6b581ff4f0b73b5683db28b6093a8b
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-06 15:31:35 +0100

    marawacc import updated

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -27,7 +27,7 @@ suite = {
     "suites" : [
             {
                "name" : "marawacc",
-               "version" : "31d82e4d16d5531fc5d6130145f7b905eab492ad",
+               "version" : "ac0ac6e18d13cfc8523801d419374a06302f1a18",
                "urls" : [{"url" : "ssh://hg@bitbucket.org/juanfumero/marawacc-dev", "kind" : "hg"}]
             },
         ],
