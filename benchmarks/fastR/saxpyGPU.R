a3aefd30be3e9267b247e01d40398dffd31629ac
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-09 17:26:10 +0100

    Saxpy benchmarks gpu/cpu versions added

--- benchmarks/fastR/saxpyGPU.R
+++ benchmarks/fastR/saxpyGPU.R
@@ -1,12 +1,12 @@
 
 ## AST-X Compiler
-## Saxpy benchmark, baseline 
+## Saxpy benchmark, GPU version
 
 ## Parse arguments
 args <- commandArgs(trailingOnly=TRUE)
 
 if (length(args) == 0) {
-	stop("No input size passed. Usage: ./saxpy.R <size> ")
+	stop("No input size passed. Usage: ./saxpyGPU.R <size> ")
 } 
 
 size <- as.integer(args[1])
@@ -17,18 +17,16 @@ REPETITIONS <- 100
 benchmark <- function(inputSize) {
 
 	saxpyFunction <- function(x, y) {
-		result <- (alpha * x) + y;
+		result <- (0.12 * x) + y;
 		return (result);
 	}	
-
-	alpha <- 0.12
 
 	x <- 0:size;
 	y <- 0:size;
 
 	for (i in 1:REPETITIONS) {
 		start <- nanotime()
-		result <- mapply(saxpyFunction, x, y);
+		result <- marawacc.testGPU(x, saxpyFunction, y);
 		total <- nanotime() - start
 		print(total)
 		#print(result);
