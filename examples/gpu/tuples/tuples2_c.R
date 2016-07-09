6b8b013489a798532158ecb4a7339a74b77b248c
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-09 17:07:19 +0100

    examples within log, sqrt added and tuples

---
+++ examples/gpu/tuples/tuples2_c.R
@@ -0,0 +1,12 @@
+#!/bin/R
+a <- 1:1000000
+
+gpuFunction <- function(x) {
+	return(list(sqrt(x), log(x * 2)))
+}
+
+for (i in seq(1,10)) {
+	result <- marawacc.testGPU(a, gpuFunction)
+}
+print(result)
+
