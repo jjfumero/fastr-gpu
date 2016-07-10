2177d7a057bc0b5e5e90babe91f90250b86f4a62
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-10 13:33:27 +0100

    Blackscholes benchmark template added

---
+++ benchmarks/fastR/blackscholes/blackscholes.R
@@ -0,0 +1,83 @@
+
+## AST-X Compiler
+## Blackcholes benchmark
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./saxpy.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 100
+
+## Lambda expression for the computation
+benchmark <- function(inputSize) {
+
+	bsFunction <- function(x) {
+		inRand <- x;
+        sLOWERLIMIT <- 10.0;
+        sUPPERLIMIT <- 100.0;
+        kLOWERLIMIT <- 10.0;
+        kUPPERLIMIT <- 100.0;
+        tLOWERLIMIT <- 1.0;
+        tUPPERLIMIT <- 10.0;
+        rLOWERLIMIT <- 0.01;
+        rUPPERLIMIT <- 0.05;
+        sIGMALOWERLIMIT <- 0.01;
+        sIGMAUPPERLIMIT <- 0.10;
+        s <- sLOWERLIMIT * inRand + sUPPERLIMIT * (1.0 - inRand);
+        KConstant <- kLOWERLIMIT * inRand + kUPPERLIMIT * (1.0 - inRand);
+        t <- tLOWERLIMIT * inRand + tUPPERLIMIT * (1.0 - inRand);
+        r <- rLOWERLIMIT * inRand + rUPPERLIMIT * (1.0 - inRand);
+        v <- sIGMALOWERLIMIT * inRand + sIGMAUPPERLIMIT * (1.0 - inRand);
+
+	 	d1 <- (log(s / KConstant) + ((r + (v * v / 2)) * t)) / v * sqrt(t)
+		d2 <- d1 - (v * sqrt(t))
+
+       a1 <- 0.319381530
+       a2 <- -0.356563782
+       a3 <- 1.781477937
+       a4 <- -1.821255978
+       a5 <- 1.330274429
+       a6 <- 2.506628273
+
+       l <- abs(d1);
+       k <- 1.0 / (1.0 + 0.2316419 * l);
+
+	   w <- 1.0 - 1.0 / 1 * a6 * exp((-1 * l) * l / 2) * (a1 * k + a2 * k * k * 1 + a3 * k * k * k * +a4 * k * k * k * k * 1 + a5 * k * k * k * k * k);
+		resultD1 <- w
+       if (d1 < 0) {
+       		resultD1 <- 1.0 - w;
+       }
+
+		# cnd(d2)
+        l <- abs(d2);
+        k <- 1.0 / (1.0 + 0.2316419 * l);
+        w <- 1.0 - 1.0 / 1 * a6 * exp((-1 * l) * l / 2) * (a1 * k + a2 * k * k * 1 + a3 * k * k * k * +a4 * k * k * k * k * 1 + a5 * k * k * k * k * k);
+        resultD2 <- w;
+        if (d2 < 0) {
+        	resultD2 <- 1.0 - w;
+        }
+
+       callRes <- s * resultD1 - KConstant * exp(1 * t * (-1) * r) * resultD2;
+ 
+		list(callRes, d2)	
+	}	
+
+	x <- 0:size;
+
+	for (i in 1:REPETITIONS) {
+		start <- nanotime()
+		result <- mapply(bsFunction, x);
+		total <- nanotime() - start
+		print(total)
+		#print(result);
+	}
+}
+
+## Main 
+benchmark(size)
+
