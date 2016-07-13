c06fdfd134fb687bce808ae6261f920f555bd3e1
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-13 13:40:01 +0100

    scope example added

---
+++ examples/scope/scope01.R
@@ -0,0 +1,25 @@
+a <- 1:1000
+
+gpuFunction <- function(x) {
+	foo <- 0
+	for (i in 1:size) {
+		b <- x * scope[i]
+		foo <- foo + b
+	}
+	return (foo)
+}
+
+# Inputs
+size <- 1000
+scope <- runif(1000)
+#scope <- 1001:2000
+a <- 1:size
+
+for (i in seq(1,20)) {
+    start <- nanotime()
+    result <- marawacc.gpusapply(a, gpuFunction)
+    end <- nanotime()
+    print(end-start);
+
+}
+
