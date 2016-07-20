4f442144212ed6cefe81e763581d4c1243a8f64a
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-20 15:55:28 +0100

    simple2 scope analysis examples added

---
+++ benchmarks/fastR/simple/simple2GPU.R
@@ -0,0 +1,46 @@
+
+## ASTx
+## NBody benchmark, baseline 
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./simpleGPU.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 10
+
+NUMBODIES <- size
+
+## Lambda expression for the computation
+benchmark <- function(inputSize) {
+
+	gpuFunction <- function(px) {
+    	dist <- 0
+	    for (i in 1:size) {
+    	    x <- px * vectorData[i]
+        	dist <- dist + x        
+	    }   
+    	return(dist)    
+	}
+
+	input <- runif(size)
+	vectorData <- runif(size)
+
+	for (i in 1:REPETITIONS) {
+		start <- nanotime()
+		result <- marawacc.testGPU(input, gpuFunction);
+		end <- nanotime()
+		total <- end - start
+		print(total)
+	}
+}
+
+## Main
+print("FASTR CPU")
+print(paste("SIZE:", size))
+benchmark(size)
+
--- benchmarks/fastR/simple/simpleGPU.R
+++ benchmarks/fastR/simple/simpleGPU.R
@@ -13,39 +13,25 @@ size <- as.integer(args[1])
 
 REPETITIONS <- 10
 
-NUMBODIES <- size
-
-## Lambda expression for the computation
-benchmark <- function(inputSize) {
-
-	nbodyFunction <- function(px) {
-		dist <- 0
-		for (i in 1:NUMBODIES) {
-			body <- 4*i
-			r <- rep(0, 3)
-			r[1] <- vectorData[i]
-			#r[1] <- vectorData[body] - px	
-			#r[2] <- vectorData[body + 1] - px
-			#r[3] <- vectorData[body + 2] - px	
-			dist <- dist + (r[1] + r[2] + r[3])	
-		}
-		return(dist)	
+gpuFunction <- function(px) {
+	dist <- 0
+	for (i in 1:size) {
+		x <- px * vectorData[i]
+		dist <- dist + x	
 	}
-
-	px <- runif(size)
-	vectorData <- runif(4*size)
-
-	for (i in 1:REPETITIONS) {
-		start <- nanotime()
-		result <- marawacc.testGPU(px, nbodyFunction);
-		end <- nanotime()
-		total <- end - start
-		print(total)
-	}
+	return(dist)	
 }
 
-## Main
-print("FASTR CPU")
+input <- runif(size)
+vectorData <- runif(size)
+
+for (i in 1:REPETITIONS) {
+	start <- nanotime()
+	result <- marawacc.testGPU(input, gpuFunction);
+	end <- nanotime()
+	total <- end - start
+	print(total)
+}
+
 print(paste("SIZE:", size))
-benchmark(size)
 
