ac91bd9f830b4b9bcc958ec5c18176c3d49da8d0
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-26 11:49:11 +0100

    benchmarks script tests added

---
+++ runBenchTest.sh
@@ -0,0 +1,6 @@
+./run benchmarks/fastR/saxpy/saxpyGPU.R 100
+./run benchmarks/fastR/blackscholes/blackscholesGPU.R 100
+./run benchmarks/fastR/montecarlo/montecarloGPU.R 100
+./run benchmarks/fastR/kmeans/kmeansGPU.R 100
+./run benchmarks/fastR/nbody/nbodyGPU.R 100
+
