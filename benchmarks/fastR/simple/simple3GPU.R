5c2d0364471857504899b47f9af6d861fc956980
Juan Fumero <juan.fumero@ed.ac.uk>
2016-08-08 09:38:52 +0100

    simple benchmark for GPU added

---
+++ benchmarks/fastR/simple/simple3GPU.R
@@ -0,0 +1,39 @@
+## ASTx
+## Simple benchmark 
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./simpleGPU.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 10
+
+gpuFunction <- function(px) {
+	dist <- 0
+	for (i in 1:size) {
+		x <- px * vectorData[i]
+		x <- x / 2
+		x <- x * vectorData2[i]
+		dist <- dist + x	
+	}
+	return(dist)	
+}
+
+input <- runif(size)
+vectorData <- runif(size)
+vectorData2 <- runif(size)
+
+for (i in 1:REPETITIONS) {
+	start <- nanotime()
+	result <- marawacc.gpusapply(input, gpuFunction);
+	end <- nanotime()
+	total <- end - start
+	print(total)
+}
+
+print(paste("SIZE:", size))
+
