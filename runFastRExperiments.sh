a3e8dd28dc40d8f9054000ae5c8027c9d5ad8d81
Juan Fumero <juan.fumero@ed.ac.uk>
2016-08-10 16:47:41 +0100

    script for benchmarks in progress

--- runFastRExperiments.sh
+++ runFastRExperiments.sh
@@ -15,18 +15,6 @@ function runSaxpy {
     done
 }
 
-function saxpy() {
-	echo "CPU"
-    getDate
-    logFile="fastr_saxpyLogs_$NOW.log"	
-	runSaxpy $logFile saxpy.R
-	
-	echo "GPU"
-	getDate
-    logFile="astx_saxpyLogs_$NOW.log"
-	runSaxpy $logFile saxpyGPU.R
-}
-
 function runBS {
     # 1048576:67108864:*2
 	logFile=$1
@@ -35,18 +23,6 @@ function runBS {
         echo "Running blackscholes with : $size" 
         ./runbench benchmarks/fastR/blackscholes/$2 $size >> $logFile
     done
-}
-
-function blackscholes() {
-	echo "FastR"
-    getDate
-    logFile="fastr_blackscholesLogs_$NOW.log"
-	runBS $logFile blackscholes.R
-
-	echo "GPU"
-    getDate
-    logFile="astx_blackscholesLogs_$NOW.log"
-	runBS $logFile blackscholesGPU.R
 }
 
 function runMC {
@@ -59,20 +35,100 @@ function runMC {
     done
 }
 
-function montecarlo() {
-	echo "FastR"
-    getDate
-    logFile="fastr_montecarloLogs_$NOW.log"
-	runMC $logFile montecarlo.R
-
-	echo "GPU"
-    getDate
-    logFile="astx_montecarloLogs_$NOW.log"
-	runMC $logFile montecarloGPU.R
+function runKmeans {
+	logFile=$1
+    for size in 4194304 8388608 16777216 33554432  
+    do
+        echo "Running kmeans with : $size" 
+        ./runbench benchmarks/fastR/kmeans/$2 $size >> $logFile
+    done
 }
 
+function runNBody {
+	logFile=$1
+    for size in 65536 131072 262144 524288 
+    do
+        echo "Running nbody with : $size" 
+        ./runbench benchmarks/fastR/nbody/$2 $size >> $logFile
+    done
+}
 
-# main
-saxpy
-blackscholes
-##montecarlo	# pending
+function runDFT {
+	logFile=$1
+    for size in 4194304 8388608 16777216 33554432 
+    do
+        echo "Running dft with : $size" 
+        ./runbench benchmarks/fastR/dft/$2 $size >> $logFile
+    done
+}
+
+function runCPU() {
+	echo "SAXPY"
+	getDate
+	logFile="fastr_saxpyLogs_$NOW.log"	
+	runSaxpy $logFile saxpy.R
+
+	echo "BS"
+    	getDate
+    	logFile="fastr_blackscholesLogs_$NOW.log"
+	runBS $logFile blackscholes.R
+
+	echo "MonteCarlo"
+    	getDate
+    	logFile="fastr_montecarloLogs_$NOW.log"
+	runMC $logFile montecarlo.R
+
+	echo "KMeans"
+    	getDate
+    	logFile="fastr_kmemansLogs_$NOW.log"	
+	runKmeans $logFile kmeans.R
+
+	echo "NBody"
+        getDate
+        logFile="fastr_nbodyLogs_$NOW.log"	
+	runNBody $logFile nbody.R
+
+	echo "DFT"
+	getDate
+    	logFile="fastr_dftLogs_$NOW.log"	
+	runDFT $logFile dft.R
+}
+
+function runGPU() {
+	echo "SAXPY"
+	getDate
+    	logFile="astx_saxpyLogs_$NOW.log"
+	runSaxpy $logFile saxpyGPU.R
+
+	echo "BS"
+	    getDate
+    	logFile="astx_blackscholesLogs_$NOW.log"
+	runBS $logFile blackscholesGPU.R
+
+	echo "MonteCarlo"
+	    getDate
+    	logFile="astx_montecarloLogs_$NOW.log"
+	runMC $logFile montecarloGPU.R
+	
+	
+	echo "KMEANS"
+	getDate
+	logFile="astx_kmeansLogs_$NOW.log"
+	runKmeans $logFile kmeansGPU.R
+	
+	echo "NBODY"
+	getDate
+	logFile="astx_nbodyLogs_$NOW.log"
+	runNBody $logFile nbodyGPU.R
+	
+	echo "DFT"
+	getDate
+	logFile="astx_dftLogs_$NOW.log"
+	runNBody $logFile dftGPU.R
+}
+
+# ###############################################
+# Run experiments
+# ###############################################
+runCPU
+runGPU
