59a2b40032e8cf7f63d7b2119d187fd887d441be
Juan Fumero <juan.fumero@ed.ac.uk>
2016-08-21 13:58:14 +0100

    run script added

---
+++ scripts/run.sh
@@ -0,0 +1,17 @@
+
+# Saxpy 8M
+./runbench benchmarks/fastR/saxpy/saxpy.R 1048576 > saxpyFASTR.log 
+./runbench benchmarks/fastR/saxpy/saxpyGPUPArrays.R 1048576 > saxpyASTx.log
+$GNUR/Rscript benchmarks/fastR/saxpy/saxpyGNU.R 1048576 > saxpyGNU.log
+
+# BS 1M
+./runbench benchmarks/fastR/blackscholes/blackscholes.R 1048576 > bsFASTR.log 
+./runbench benchmarks/fastR/blackscholes/blackscholesGPUPArrays.R 1048576 > bsASTx.log
+$GNUR/Rscript benchmarks/fastR/blackscholes/blackscholesGNU.R 1048576 > bsGNU.log
+
+# BS 4M
+./runbench benchmarks/fastR/blackscholes/blackscholes.R 4194304 > bsFASTR4.log 
+./runbench benchmarks/fastR/blackscholes/blackscholesGPUPArrays.R 4194304 > bsASTx4.log
+$GNUR/Rscript benchmarks/fastR/blackscholes/blackscholesGNU.R 4194304 > bsGNU4.log
+
+
