e4ce342a198d3c106bf2a29c602ac35e6edbba17
Juan Fumero <juan.fumero@ed.ac.uk>
2016-08-27 12:25:44 +0100

    kmeans parrays added

---
+++ benchmarks/fastR/kmeans/kmeansGPUPArrays.R
@@ -0,0 +1,87 @@
+
+## ASTx
+## KMeans benchmark
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./kmeansGPUPArrays.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 11
+KS <- 10
+
+CHECK_RESULT <- TRUE
+
+benchmark <- function(inputSize) {
+
+	kmeansOpenCLFunction <- function(x, y) {
+		minDist <- 10000000
+		id <- 0
+		for (i in 1:KS) {
+			currentDist <- (x - centre[i]) * (x - centre[i])  +
+						   (y - centre[i + KS]) * (y - centre[i + KS])
+			if (currentDist < minDist) {
+				minDist <- currentDist
+				id <- i
+			} else {
+			}	
+		}
+		return(id)
+	}
+
+	kmeansFastRFunction <- function(x, y) {
+		minDist <- 10000000
+		id <- -1
+		for (i in 1:KS) {
+			currentDist <- (x - centre[i]) * (x - centre[i]) + 
+						   (y - centre[i + KS]) * (y - centre[i + KS])
+			if (currentDist < minDist) {
+				minDist <- currentDist
+				id <- i
+			}			
+		}
+		return(id)
+	}
+
+	centre <<- runif(KS*2)  * 10 * 2 - 10 
+	x <- centre[1:KS]
+	y <- centre[(KS+1):(KS*2)]
+
+	if (CHECK_RESULT) {
+		resultSeq <- mapply(kmeansFastRFunction, x, y);
+	}
+
+	x <- marawacc.parray(x)	
+	y <- marawacc.parray(y)	
+
+	for (i in 1:REPETITIONS) {
+		start <- nanotime()
+		result <- marawacc.gpusapply(x, kmeansOpenCLFunction, y);
+		end <- nanotime()
+		total <- end - start
+		print(paste("Total Time: ", total))
+
+		if (CHECK_RESULT) {
+			nonError <- identical(resultSeq, result)
+			correct <- TRUE
+			if (!nonError) {
+				for (i in seq(result)) {
+					if (abs(resultSeq[i] - result[i]) > 0.1) {
+						print("Result is wrong")
+						correct <- FALSE
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+## Main
+print("ASTx GPU")
+print(paste("SIZE:", size))
+benchmark(size)
+
