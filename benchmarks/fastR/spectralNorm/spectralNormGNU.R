a6f2399f2d8e34a141ccc8e84d82f0a8ebe0183f
Juan Fumero <juan.fumero@ed.ac.uk>
2016-08-28 13:21:02 +0100

    spectral norm R versions added

---
+++ benchmarks/fastR/spectralNorm/spectralNormGNU.R
@@ -0,0 +1,67 @@
+
+## GNU R
+## SpectralNorm benchmark 
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./spectralNormGNU.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 11
+
+require(compiler)
+enableJIT(3)
+
+## Lambda expression for the computation
+benchmark <- function(inputSize) {
+
+	spectralNorm1 <- function(i) {
+		sum <- 0
+		for (j in 1:size) {
+			evalAPrime <- (i + j) * (i + j + 1)
+			evalA <- evalAPrime / 2
+			evalA <- evalA + i + 1
+			evalA <- 1.0/ evalA
+			partial <- evalA * v[j]
+			sum <- partial + sum
+		}
+		return(sum)
+	}
+
+	spectralNorm2 <- function(i) {
+		sum <- 0
+		for (j in 1:size) {
+			evalAPrime <- (j + i) * (j + i + 1)
+			evalA <- evalAPrime / 2
+			evalA <- evalA + j + 1
+			evalA <- 1.0/ evalA
+			partial <- evalA * v[j]
+			sum <- partial + sum
+		}
+		return(sum)
+	}
+
+	v <<- rep(1, size)
+	x <- 1:size
+
+	for (i in 1:REPETITIONS) {
+		start <- proc.time()
+
+		resultA <- mapply(spectralNorm1, x);
+		v <<- resultA
+		resultB <- mapply(spectralNorm2, resultA) 
+
+		end <- proc.time()
+		total <- end - start
+		print(total)
+	}
+}
+
+## Main
+print("FASTR CPU")
+print(paste("SIZE:", size))
+benchmark(size)
+
---
+++ benchmarks/fastR/spectralNorm/spectralNormSeq.R
@@ -0,0 +1,63 @@
+
+## ASTx
+## SpectralNorm benchmark 
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./spectralNormSeq.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 11
+
+benchmark <- function(inputSize) {
+
+	spectralNorm1 <- function(i) {
+		sum <- 0
+		for (j in 1:size) {
+			evalAPrime <- (i + j) * (i + j + 1)
+			evalA <- evalAPrime / 2
+			evalA <- evalA + i + 1
+			evalA <- 1.0/ evalA
+			partial <- evalA * v[j]
+			sum <- partial + sum
+		}
+		return(sum)
+	}
+
+	spectralNorm2 <- function(i) {
+		sum <- 0
+		for (j in 1:size) {
+			evalAPrime <- (j + i) * (j + i + 1)
+			evalA <- evalAPrime / 2
+			evalA <- evalA + j + 1
+			evalA <- 1.0/ evalA
+			partial <- evalA * v[j]
+			sum <- partial + sum
+		}
+		return(sum)
+	}
+
+	v <<- rep(1, size)
+	x <- 1:size
+
+	for (i in 1:REPETITIONS) {
+		start <- nanotime()
+
+		resultA <- marawacc.sapply(x, spectralNorm1, nThreads=1);
+		v <<- resultA
+		resultB <- mapply(resultA, spectralNorm2,  nThreads=1) 
+
+		end <- nanotime()
+		total <- end - start
+		print(total)
+	}
+}
+
+## Main
+print(paste("SIZE:", size))
+benchmark(size)
+
