57c18ed5f318ce2cc3ed84466222bddf45b73c95
Juan Fumero <juan.fumero@ed.ac.uk>
2016-08-31 20:22:50 +0100

    organized script

--- scripts/freshRuns.py
+++ scripts/freshRuns.py
@@ -20,20 +20,34 @@ def execute(bench, sizes, version, symName):
         if (not DEBUG):
             os.system(command)
 
-def saxpy():
-    # Saxpy 8M
-    bench="saxpy"
-    sizes = [2097152, 4194304, 8388608, 16777216, 33554432]
-
-    versions = ["saxpy.R", "saxpySeq.R", "saxpyGPU.R", "saxpyGPUPArrays"]
-    symbolicNames = ["saxpyFastR", "saxpySeq", "saxpyASTxFull", "saxpyASTx"]
-
+def runExperiment(bench, sizes, versions, symbolicNames):
     for i in range(len(versions)):
         version = versions[i]
         symbolicName = symbolicNames[i]
         execute(bench, sizes, version, symbolicName)
 
 
+def saxpy():
+    bench="saxpy"
+    sizes = [2097152, 4194304, 8388608, 16777216, 33554432]
+
+    versions = ["saxpy.R", "saxpySeq.R", "saxpyGPU.R", "saxpyGPUPArrays"]
+    symbolicNames = ["saxpyFastR", "saxpySeq", "saxpyASTxFull", "saxpyASTx"]
+
+    runExperiment(bench, sizes, versions, symbolicNames)
+
+def blacksholes():
+    bench = "blacksholes"
+    sizes = [1, 2, 3, 4, 5]
+
+    versions = ["blackcholes.R", "blackcholesSeq.R", "blackcholesGPU.R", "blackcholesGPUPArrays"]
+    symbolicNames = ["blackcholesFastR", "blackcholesSeq", "blackcholesASTxFull", "blackcholesASTx"]
+    
+    runExperiment(bench, sizes, versions, symbolicNames)
+   
 if __name__ == "__main__":
     
     saxpy()
+    blacksholes()
+
+
