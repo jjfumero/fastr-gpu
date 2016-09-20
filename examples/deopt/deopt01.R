02552cec1d076179781c554d6a5f327c1b110d00
Juan Fumero <juan.fumero@ed.ac.uk>
2016-09-20 15:02:02 +0100

    deopt GPU R example added

---
+++ examples/deopt/deopt01.R
@@ -0,0 +1,43 @@
+
+## AST-X Compiler
+## Saxpy benchmark, GPU version
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./deopt01.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 11
+
+## Lambda expression for the computation
+benchmark <- function(inputSize) {
+
+	deoptSample <- function(x) {
+		if (x == 1) {
+			result <- 0
+		} else {
+			result <- 1
+		}
+		return (result);
+	}	
+
+	x <- rep(1, size)
+	x[size] = 100
+
+	for (i in 1:REPETITIONS) {
+		start <- nanotime()
+		result <- marawacc.gpusapply(x, deoptSample);
+		total <- nanotime() - start
+		print(paste("Total Time: ", total))
+	}
+}
+
+## Main 
+print("ASTx GPU")
+print(paste("SIZE:", size))
+benchmark(size)
+
