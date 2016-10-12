376e2282b457e64b23149c28ebb16b1543a8ad2b
Juan Fumero <juan.fumero@ed.ac.uk>
2016-10-12 10:46:38 +0100

    run script updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 
 function saxpy {
-	echo "SAXY"
+	echo "SAXPY"
 	size=8388608
 	#./runbench benchmarks/fastR/saxpy/saxpy.R $size > $DIRECTORY/saxpyFASTR.log 
 	#./runbench benchmarks/fastR/saxpy/saxpySeq.R $size > $DIRECTORY/saxpySeq.log
@@ -21,72 +21,79 @@ function bs {
 
 function nbody {
 	echo "NBODY"
-	#./runbench benchmarks/fastR/nbody/nbody.R 65536 > nbodyFastR.log
-	#./runbench benchmarks/fastR/nbody/nbodySeq.R 65536 > nbodySeq.log
-	./runbench benchmarks/fastR/nbody/nbodyGPU.R 65536 > $DIRECTORY/nbodyASTxFull.log
-	#./runbench benchmarks/fastR/nbody/nbodyGPUPArrays.R 65536 > nbodyASTx.log
-	#$GNUR/Rscript benchmarks/fastR/nbody/nbodyGNU.R 65536 > nbodyGNU.log
+	size=65536
+	#./runbench benchmarks/fastR/nbody/nbody.R $size > $DIRECTORY/nbodyFastR.log
+	#./runbench benchmarks/fastR/nbody/nbodySeq.R $size > $DIRECTORY/nbodySeq.log
+	./runbench benchmarks/fastR/nbody/nbodyGPU.R $size > $DIRECTORY/nbodyASTxFull.log
+	#./runbench benchmarks/fastR/nbody/nbodyGPUPArrays.R $size > $DIRECTORY/nbodyASTx.log
+	#$GNUR/Rscript benchmarks/fastR/nbody/nbodyGNU.R $size > $DIRECTORY/nbodyGNU.log
 }
 
 function dft() {
 	echo "DFT"
-	#./runbench benchmarks/fastR/dft/dft.R 8192 > dftFastR.log
-	#./runbench benchmarks/fastR/dft/dftSeq.R 8192 > dftSeq.log
+	size=8192
+	#./runbench benchmarks/fastR/dft/dft.R $size > $DIRECTORY/dftFastR.log
+	#./runbench benchmarks/fastR/dft/dftSeq.R $size > $DIRECTORY/dftSeq.log
 	./runbench benchmarks/fastR/dft/dftGPU.R 8192 > $DIRECTORY/dftASTxFull.log
-	#./runbench benchmarks/fastR/dft/dftGPUPArrays.R 8192 > dftASTx.log
-	#$GNUR/Rscript benchmarks/fastR/dft/dftGNU.R 8192 > dftGNU.log
+	#./runbench benchmarks/fastR/dft/dftGPUPArrays.R $size > $DIRECTORY/dftASTx.log
+	#$GNUR/Rscript benchmarks/fastR/dft/dftGNU.R $size > $DIRECTORY/dftGNU.log
 }
 
 function mandelbrot() {
 	echo "MANDELBROT"
-	#./runbench benchmarks/fastR/mandelbrot/mandelbrot.R 1024 > mandelbrotFastR.log
-	#./runbench benchmarks/fastR/mandelbrot/mandelbrotSeq.R 1024 > mandelbrotSeq.log
-	./runbench benchmarks/fastR/mandelbrot/mandelbrotGPU.R 1024 > $DIRECTORY/mandelbrotASTxFull.log
-	#./runbench benchmarks/fastR/mandelbrot/mandelbrotGPUPArrays.R 1024 > mandelbrotASTx.log
-	#$GNUR/Rscript benchmarks/fastR/mandelbrot/mandelbrotGNU.R 1024 > mandelbrotGNU.log
+	size=1024
+	#./runbench benchmarks/fastR/mandelbrot/mandelbrot.R $size > $DIRECTORY/mandelbrotFastR.log
+	#./runbench benchmarks/fastR/mandelbrot/mandelbrotSeq.R $size > $DIRECTORY/mandelbrotSeq.log
+	./runbench benchmarks/fastR/mandelbrot/mandelbrotGPU.R $size > $DIRECTORY/mandelbrotASTxFull.log
+	#./runbench benchmarks/fastR/mandelbrot/mandelbrotGPUPArrays.R $size > $DIRECTORY/mandelbrotASTx.log
+	#$GNUR/Rscript benchmarks/fastR/mandelbrot/mandelbrotGNU.R $size > $DIRECTORY/mandelbrotGNU.log
 }
 
 function montecarlo() {
 	echo "MONTECARLO"
-	#./runbench benchmarks/fastR/montecarlo/montecarlo.R  262144 > montecarloFastR.log
-	#./runbench benchmarks/fastR/montecarlo/montecarloSeq.R  262144 > montecarloSeq.log
-	./runbench benchmarks/fastR/montecarlo/montecarloGPU.R  262144 > $DIRECTORY/montecarloASTxFull.log
-	#./runbench benchmarks/fastR/montecarlo/montecarloGPUPArrays.R  262144 > montecarloASTx.log
-	#$GNUR/Rscript benchmarks/fastR/montecarlo/montecarloGNU.R 262144 > montecarloGNU.log
+	size=262144
+	#./runbench benchmarks/fastR/montecarlo/montecarlo.R  $size > $DIRECTORY/montecarloFastR.log
+	#./runbench benchmarks/fastR/montecarlo/montecarloSeq.R  $size > $DIRECTORY/montecarloSeq.log
+	./runbench benchmarks/fastR/montecarlo/montecarloGPU.R  $size > $DIRECTORY/montecarloASTxFull.log
+	#./runbench benchmarks/fastR/montecarlo/montecarloGPUPArrays.R  $size > $DIRECTORY/montecarloASTx.log
+	#$GNUR/Rscript benchmarks/fastR/montecarlo/montecarloGNU.R $size > $DIRECTORY/montecarloGNU.log
 }
 
 function kmeans() {
 	echo "KMEANS"
-	#./runbench benchmarks/fastR/kmeans/kmeans.R 4194304 > kmeansFastR.log
-	#./runbench benchmarks/fastR/kmeans/kmeansSeq.R 4194304 > kmeansSeq.log
-	./runbench benchmarks/fastR/kmeans/kmeansGPU.R 4194304 > $DIRECTORY/kmeansASTxFull.log
-	#./runbench benchmarks/fastR/kmeans/kmeansGPUPArrays.R 4194304 > kmeansASTx.log
-	#$GNUR/Rscript benchmarks/fastR/kmeans/kmeansGNU.R 4194304 > kmeansGNU.log
+	size=4194304
+	#./runbench benchmarks/fastR/kmeans/kmeans.R $size > $DIRECTORY/kmeansFastR.log
+	#./runbench benchmarks/fastR/kmeans/kmeansSeq.R $size > $DIRECTORY/kmeansSeq.log
+	./runbench benchmarks/fastR/kmeans/kmeansGPU.R $size > $DIRECTORY/kmeansASTxFull.log
+	#./runbench benchmarks/fastR/kmeans/kmeansGPUPArrays.R $size > $DIRECTORY/kmeansASTx.log
+	#$GNUR/Rscript benchmarks/fastR/kmeans/kmeansGNU.R $size > $DIRECTORY/kmeansGNU.log
 }
 
 function hilbert() {
 	echo "HILBERT"
-	#./runbench benchmarks/fastR/hilbert/hilbert.R 4096 > hilbertFastR.log
-	#./runbench benchmarks/fastR/hilbert/hilbertSeq.R 4096 > hilbertSeq.log
-	./runbench benchmarks/fastR/hilbert/hilbertGPU.R 4096 > $DIRECTORY/hilbertASTxFull.log
-	#./runbench benchmarks/fastR/hilbert/hilbertGPUPArrays.R 4096 > hilbertASTx.log
-	#$GNUR/Rscript benchmarks/fastR/hilbert/hilbertGNU.R 4096 > hilbertGNU.log
+	size=4096
+	#./runbench benchmarks/fastR/hilbert/hilbert.R $size > $DIRECTORY/hilbertFastR.log
+	#./runbench benchmarks/fastR/hilbert/hilbertSeq.R $size > $DIRECTORY/hilbertSeq.log
+	./runbench benchmarks/fastR/hilbert/hilbertGPU.R $size > $DIRECTORY/hilbertASTxFull.log
+	#./runbench benchmarks/fastR/hilbert/hilbertGPUPArrays.R $size > $DIRECTORY/hilbertASTx.log
+	#$GNUR/Rscript benchmarks/fastR/hilbert/hilbertGNU.R $size > $DIRECTORY/hilbertGNU.log
 }
 
 function spectralNorm() {
 	echo "SPECTRAL NORM"
-	#./runbench benchmarks/fastR/spectralNorm/spectralNorm.R 32768 > spectralNormFastR.log
-	#./runbench benchmarks/fastR/spectralNorm/spectralNormSeq.R 32768 > spectralNormSeq.log
-	./runbench benchmarks/fastR/spectralNorm/spectralNormGPU.R 32768 > $DIRECTORY/spectralNormASTxFull.log
-	#./runbench benchmarks/fastR/spectralNorm/spectralNormGPUPArrays.R 32768 > spectralNormASTx.log
-	#$GNUR/Rscript benchmarks/fastR/spectralNorm/spectralNormGNU.R 32768 > spectralNormGNU.log
+	size=32768
+	#./runbench benchmarks/fastR/spectralNorm/spectralNorm.R $size > $DIRECTORY/spectralNormFastR.log
+	#./runbench benchmarks/fastR/spectralNorm/spectralNormSeq.R $size > $DIRECTORY/spectralNormSeq.log
+	./runbench benchmarks/fastR/spectralNorm/spectralNormGPU.R $size > $DIRECTORY/spectralNormASTxFull.log
+	#./runbench benchmarks/fastR/spectralNorm/spectralNormGPUPArrays.R $size > $DIRECTORY/spectralNormASTx.log
+	#$GNUR/Rscript benchmarks/fastR/spectralNorm/spectralNormGNU.R $size > $DIRECTORY/spectralNormGNU.log
 }
 
 DIRECTORY=`date +%Y-%m-%d_%H_%M_%S`
 
 mkdir $DIRECTORY
 
-## main
+## Run the benchmarks
 saxpy
 bs
 nbody
