d21cdc1a2847822fecd7f13e0ba5ab2efa4308d3
Juan Fumero <juan.fumero@ed.ac.uk>
2016-11-01 14:43:40 +0000

    matrixVector benchmark added

---
+++ benchmarks/fastR/matrixVector/matrixVector.R
@@ -0,0 +1,46 @@
+
+## AST-X Compiler
+## Matrix Vector multiplication
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./matrixVector.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 11
+
+CHECK_RESULT <- FALSE
+
+## Lambda expression for the computation
+benchmark <- function(inputSize) {
+
+	matrixVectorMultiplication <- function(idx) {
+
+		result <- 0.0
+		for (jdx in 1:size) {
+			result <- result + _matrix[idx * size + jdx] * _vector[jdx]
+		}
+		return(result)
+	}	
+
+	index <- 1:size
+	_vector <<- runif(size)	
+	_matrix <<- runif(size*size)	
+
+	for (i in 1:REPETITIONS) {
+		start <- nanotime()
+		result <- mapply(matrixVectorMultiplication, index)
+		total <- nanotime() - start
+		print(paste("Total Time: ", total))
+	}
+}
+
+## Main 
+print("FASTR CPU")
+print(paste("SIZE:", size))
+benchmark(size)
+
---
+++ benchmarks/fastR/matrixVector/matrixVectorGPU.R
@@ -0,0 +1,70 @@
+## AST-X Compiler
+## Matrix Vector multiplication
+
+## Parse arguments
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+	stop("No input size passed. Usage: ./matrixVector.R <size> ")
+} 
+
+size <- as.integer(args[1])
+
+REPETITIONS <- 11
+CHECK_RESULT <- TRUE
+
+## Lambda expression for the computation
+benchmark <- function(inputSize) {
+
+	matrixVectorMultiplicationGPU <- function(idx) {
+		result <- 0.0
+		for (jdx in 1:size) {
+			m <- _matrix[idx * size + jdx]
+			v <- _vector[jdx]
+			r <- m * v			
+			result <- result + r
+		}
+		return(result)
+	}
+	
+	matrixVectorMultiplicationCPU <- function(idx) {
+		result <- 0.0
+		for (jdx in 1:size) {
+			result <- result + _matrix[idx * size + jdx] * _vector[jdx]
+		}
+		return(result)
+	}	
+
+	index <- 1:size
+	_vector <<- runif(size)	
+	_matrix <<- runif(size * size)	
+
+	if (CHECK_RESULT) {
+		resultSeq <- mapply(matrixVectorMultiplicationCPU, index)
+	}
+
+	for (i in 1:REPETITIONS) {
+		print("Running benchmark")
+		system.gc()
+		start <- nanotime()
+		result <- marawacc.testGPU(index, matrixVectorMultiplicationGPU)
+		total <- nanotime() - start
+		print(paste("Total Time: ", total))
+
+		if (CHECK_RESULT) {
+			for (j in 1:size) {
+				if (abs(resultSeq[j] - result[j] > 0.1)) {
+					print("Result is wrong")
+					break
+				}
+			}
+		}
+
+	}
+}
+
+## Main 
+print("FASTR CPU")
+print(paste("SIZE:", size))
+benchmark(size)
+
