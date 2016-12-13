d409dd5169485b95f95387d2c32f95ce6e458c7d
Juan Fumero <juan.fumero@ed.ac.uk>
2016-12-13 17:34:14 +0000

    RepetitionsOfSequences moved

--- com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/sequences/RepetitionsOfSequences.java
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/gpu/sequences/RepetitionsOfSequences.java
@@ -6,7 +6,7 @@ import com.oracle.truffle.r.runtime.data.RDataFactory;
 import com.oracle.truffle.r.runtime.data.RIntSequence;
 import com.oracle.truffle.r.runtime.data.model.RAbstractVector;
 
-public abstract class RepetitionsOfRepetitions extends RExternalBuiltinNode.Arg3 {
+public abstract class RepetitionsOfSequences extends RExternalBuiltinNode.Arg3 {
 
     @SuppressWarnings("unused")
     private static int[] createSequence(int start, int max, int repetitions) {
